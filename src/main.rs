3190624ac1869230c2813b886883ffcfc4e441cf
Joel Schofield <joel@jetcharge.com.au>
2022-01-24 19:58:26 +1100

    Implement keyboard layout

--- src/main.rs
+++ src/main.rs
@@ -33,16 +33,27 @@ mod app {
     //use usb_device::device::UsbDeviceState;
 
     const SCAN_TIME_US: u32 = 1000;
+    const NUM_COLUMNS: usize = 16;
+    const NUM_ROWS: usize = 5;
     static mut USB_BUS: Option<usb_device::bus::UsbBusAllocator<rp_pico::hal::usb::UsbBus>> = None;
+
+    /*
+    [ Keycode.ESCAPE, Keycode.ONE, Keycode.TWO, Keycode.THREE, Keycode.FOUR, Keycode.FIVE, Keycode.SIX, Keycode.SEVEN, Keycode.EIGHT, Keycode.NINE, Keycode.ZERO, Keycode.MINUS, Keycode.EQUALS, None, Keycode.BACKSPACE, Keycode.DELETE ],
+    [ Keycode.TAB, None, Keycode.Q, Keycode.W, Keycode.E, Keycode.R, Keycode.T, Keycode.Y, Keycode.U, Keycode.I, Keycode.O, Keycode.P, Keycode.LEFT_BRACKET, Keycode.RIGHT_BRACKET, Keycode.BACKSLASH, Keycode.PRINT_SCREEN ],
+    [ Keycode.CAPS_LOCK , None, Keycode.A, Keycode.S, Keycode.D, Keycode.F, Keycode.G, Keycode.H, Keycode.J, Keycode.K, Keycode.L, Keycode.SEMICOLON, Keycode.QUOTE, Keycode.ENTER, None, Keycode.UP_ARROW ],
+    [ Keycode.LEFT_SHIFT, None, Keycode.Z, Keycode.X, Keycode.C, Keycode.V, Keycode.B, Keycode.N, Keycode.M, Keycode.COMMA, Keycode.PERIOD, Keycode.FORWARD_SLASH, None, Keycode.RIGHT_SHIFT, None, Keycode.DOWN_ARROW ],
+    [ Keycode.LEFT_CONTROL, Keycode.LEFT_GUI, Keycode.LEFT_ALT, None, None, None, Keycode.SPACEBAR, None, None, None, Keycode.RIGHT_ALT, function_key_layer_hold(2), None, Keycode.APPLICATION, Keycode.RIGHT_CONTROL, function_key_layer_hold(1) ] ] 
+    */
 
     #[rustfmt::skip]
     pub static LAYERS: keyberon::layout::Layers = keyberon::layout::layout! {
-        {[ // 0
-            Q W E R T Y
-        ]}
-        {[ // 1
-        1 2 3 4 5 6
-        ]}
+        {
+            [ Escape     1 2 3 4 5 6 7 8 9 0 - =  BSpace  Delete  ]
+            [ Tab      n Q W E R T Y U I O P '[' ']' '\\' PScreen ]
+            [ CapsLock n A S D F G H J K L ; '\'' Enter n Up      ]
+            [ LShift   n Z X C V B N M , . / n   RShift n Down    ]
+            [ LCtrl LGui LAlt n n n Space n n n RAlt n n Application RCtrl n ]
+        }
     };
 
     #[shared]
@@ -58,10 +69,10 @@ mod app {
         #[lock_free]
         watchdog: hal::watchdog::Watchdog,
         #[lock_free]
-        matrix: Matrix<DynPin, DynPin, 6, 1>,
+        matrix: Matrix<DynPin, DynPin, NUM_COLUMNS, NUM_ROWS>,
         layout: Layout,
         #[lock_free]
-        debouncer: Debouncer<PressedKeys<6, 1>>
+        debouncer: Debouncer<PressedKeys<NUM_COLUMNS, NUM_ROWS>>
     }
 
     #[local]
@@ -94,9 +105,27 @@ mod app {
 /*
 # The Physical Pins
 #                       COL0       COL1       COL2       COL3       COL4       COL5       COL6       COL7       COL8        COL9        COL10       COL11       COL12       COL13       COL14       COL15
-keyboard_cols = [ board.GP0, board.GP1, board.GP2, board.GP3, board.GP6, board.GP7, board.GP8, board.GP9, board.GP10, board.GP11, board.GP12, board.GP14, board.GP15, board.GP16, board.GP6, board.GP18 ]
+keyboard_cols = [ board.GP0, board.GP1, board.GP2, board.GP3, board.GP6, board.GP7, board.GP8, board.GP9, board.GP10, board.GP11, board.GP12, board.GP14, board.GP15, board.GP16, board.GP17, board.GP18 ]
 #                       ROW0        ROW1        ROW2        ROW3        ROW4
 keyboard_rows = [ board.GP19, board.GP20, board.GP21, board.GP22, board.GP26 ]
+
+Keyboard_Layout = [ [ [ Keycode.ESCAPE, Keycode.ONE, Keycode.TWO, Keycode.THREE, Keycode.FOUR, Keycode.FIVE, Keycode.SIX, Keycode.SEVEN, Keycode.EIGHT, Keycode.NINE, Keycode.ZERO, Keycode.MINUS, Keycode.EQUALS, None, Keycode.BACKSPACE, Keycode.DELETE ],
+                      [ Keycode.TAB, None, Keycode.Q, Keycode.W, Keycode.E, Keycode.R, Keycode.T, Keycode.Y, Keycode.U, Keycode.I, Keycode.O, Keycode.P, Keycode.LEFT_BRACKET, Keycode.RIGHT_BRACKET, Keycode.BACKSLASH, Keycode.PRINT_SCREEN ],
+                      [ Keycode.CAPS_LOCK , None, Keycode.A, Keycode.S, Keycode.D, Keycode.F, Keycode.G, Keycode.H, Keycode.J, Keycode.K, Keycode.L, Keycode.SEMICOLON, Keycode.QUOTE, Keycode.ENTER, None, Keycode.UP_ARROW ],
+                      [ Keycode.LEFT_SHIFT, None, Keycode.Z, Keycode.X, Keycode.C, Keycode.V, Keycode.B, Keycode.N, Keycode.M, Keycode.COMMA, Keycode.PERIOD, Keycode.FORWARD_SLASH, None, Keycode.RIGHT_SHIFT, None, Keycode.DOWN_ARROW ],
+                      [ Keycode.LEFT_CONTROL, Keycode.LEFT_GUI, Keycode.LEFT_ALT, None, None, None, Keycode.SPACEBAR, None, None, None, Keycode.RIGHT_ALT, function_key_layer_hold(2), None, Keycode.APPLICATION, Keycode.RIGHT_CONTROL, function_key_layer_hold(1) ] ],
+                    
+                    [ [ Keycode.GRAVE_ACCENT, Keycode.F1, Keycode.F2, Keycode.F3, Keycode.F4, Keycode.F5, Keycode.F6, Keycode.F7, Keycode.F8, Keycode.F9, Keycode.F10, Keycode.F11, Keycode.F12, None, None, Keycode.HOME ],
+                      [ macro1(), None, None, Keycode.UP_ARROW, None, None, None, None, None, None, None, None, None, None, None, Keycode.END ],
+                      [ Keycode.KEYPAD_NUMLOCK, None, Keycode.LEFT_ARROW, Keycode.DOWN_ARROW, Keycode.RIGHT_ARROW, None, None, None, None, None, None, None, None, Keycode.INSERT, None, Keycode.PAGE_UP],
+                      [ Keycode.LEFT_SHIFT, None, None, None, None, None, None, None, None, None, None, None, None, None, None, Keycode.PAGE_DOWN],
+                      [ Keycode.LEFT_CONTROL, Keycode.SCROLL_LOCK, None, None, None, None, Keycode.SPACEBAR, None, None, None, Keycode.RIGHT_ALT, None, None, Keycode.APPLICATION, Keycode.RIGHT_CONTROL, function_key_layer_hold(1) ] ],
+                      
+                    [ [ board_reload(), None, None, None, None, None, None, None, None, None, None, None, None, None, None, ConsumerControlCode.SCAN_PREVIOUS_TRACK],
+                      [ None, None, lighting_mode(-1), lighting_toggle_on_off(), lighting_mode(1), None, None, None, None, None, None, None, None, None, None, ConsumerControlCode.SCAN_NEXT_TRACK],
+                      [ None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, ConsumerControlCode.VOLUME_INCREMENT],
+                      [ None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, ConsumerControlCode.VOLUME_DECREMENT],
+                      [ None, None, None, None, None, None, ConsumerControlCode.PLAY_PAUSE, None, None, None, None, function_key_layer_hold(2), None, None, None, ConsumerControlCode.MUTE] ] ]
 
 # The Pin Matrix
 keyboard_cols_array = []
@@ -116,59 +145,67 @@ for pin in keyboard_rows:
     key_pin.pull = digitalio.Pull.DOWN
     keyboard_rows_array.append(key_pin)
 */
+        let gpio_col0 = pins.gpio0;
+        let gpio_col1 = pins.gpio1;
         let gpio_col2 = pins.gpio2;
         let gpio_col3 = pins.gpio3;
         let gpio_col4 = pins.gpio6;
         let gpio_col5 = pins.gpio7;
         let gpio_col6 = pins.gpio8;
         let gpio_col7 = pins.gpio9;
+        let gpio_col8 = pins.gpio10;
+        let gpio_col9 = pins.gpio11;
+        let gpio_col10 = pins.gpio12;
+        let gpio_col11 = pins.gpio14;
+        let gpio_col12 = pins.gpio15;
+        let gpio_col13 = pins.gpio16;
+        let gpio_col14 = pins.gpio17;
+        let gpio_col15 = pins.gpio18;
 
+        let gpio_row0 = pins.gpio19;
         let gpio_row1 = pins.gpio20;
-
-        // 6 input pins and 1 empty pin that is not really used, but
-        // is needed by keyberon as a "row"
-        //let gpio2 = pins.gpio2;
-        //let gpio28 = pins.gpio28;
-        //let gpio3 = pins.gpio3;
-        //let gpio27 = pins.gpio27;
-        //let gpio4 = pins.gpio4;
-        //let gpio5 = pins.gpio5;
-        //let gpio26 = pins.gpio26;
-        //let gpio6 = pins.gpio6;
-        //let gpio22 = pins.gpio22;
-        //let gpio7 = pins.gpio7;
-        //let gpio10 = pins.gpio10;
-        //let gpio11 = pins.gpio11;
-        //let gpio12 = pins.gpio12;
-        //let gpio21 = pins.gpio21;
-        //let gpio13 = pins.gpio13;
-        //let gpio15 = pins.gpio15;
-        //let gpio14 = pins.gpio14;
-
-        //let gpio20 = pins.gpio20;
+        let gpio_row2 = pins.gpio21;
+        let gpio_row3 = pins.gpio22;
+        let gpio_row4 = pins.gpio26;
         
         // delay for power on
         for _ in 0..1000 {
             cortex_m::asm::nop();
         }
 
-        let matrix: Matrix<DynPin, DynPin, 6, 1> = cortex_m::interrupt::free(move |_cs| {
+        let matrix: Matrix<DynPin, DynPin, NUM_COLUMNS, NUM_ROWS> = cortex_m::interrupt::free(move |_cs| {
             Matrix::new(
                 [
+                    gpio_col0.into_pull_up_input().into(),
+                    gpio_col1.into_pull_up_input().into(),
                     gpio_col2.into_pull_up_input().into(),
                     gpio_col3.into_pull_up_input().into(),
                     gpio_col4.into_pull_up_input().into(),
                     gpio_col5.into_pull_up_input().into(),
                     gpio_col6.into_pull_up_input().into(),
-                    gpio_col7.into_pull_up_input().into()
+                    gpio_col7.into_pull_up_input().into(),
+                    gpio_col8.into_pull_up_input().into(),
+                    gpio_col9.into_pull_up_input().into(),
+                    gpio_col10.into_pull_up_input().into(),
+                    gpio_col11.into_pull_up_input().into(),
+                    gpio_col12.into_pull_up_input().into(),
+                    gpio_col13.into_pull_up_input().into(),
+                    gpio_col14.into_pull_up_input().into(),
+                    gpio_col15.into_pull_up_input().into()
                 ],
-                [gpio_row1.into_push_pull_output().into()],
+                [
+                    gpio_row0.into_push_pull_output().into(),
+                    gpio_row1.into_push_pull_output().into(),
+                    gpio_row2.into_push_pull_output().into(),
+                    gpio_row3.into_push_pull_output().into(),
+                    gpio_row4.into_push_pull_output().into()
+                ],
             )
         })
         .unwrap();
 
         let layout = Layout::new(LAYERS);
-        let debouncer: keyberon::debounce::Debouncer<keyberon::matrix::PressedKeys<6, 1>> =
+        let debouncer: keyberon::debounce::Debouncer<keyberon::matrix::PressedKeys<NUM_COLUMNS, NUM_ROWS>> =
             Debouncer::new(PressedKeys::default(), PressedKeys::default(), 30);
 
         let mut timer = hal::Timer::new(c.device.TIMER, &mut resets);
