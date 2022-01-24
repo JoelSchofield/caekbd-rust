0eec2d926ee70c2a23e013c576096febee34ba89
Joel Schofield <joel@jetcharge.com.au>
2022-01-24 19:13:27 +1100

    Experiment with LED timings and 'smart-leds' module

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use panic_halt as _;
 
+mod hack;
+
 #[rtic::app(device = rp_pico::hal::pac, peripherals = true)]
 mod app {
 
@@ -12,25 +14,19 @@ mod app {
         hal::{self, clocks::init_clocks_and_plls, watchdog::Watchdog, Sio},
         XOSC_CRYSTAL_FREQ,
     };
-    //use embedded_hal::digital::v2::InputPin;
-    //use embedded_hal::digital::v2::OutputPin;
+    use embedded_hal::digital::v2::{InputPin, OutputPin};
     use embedded_time::duration::units::*;
-    //use rp_pico::hal::clocks::init_clocks_and_plls;
     use rp_pico::hal::gpio::DynPin;
-    //use rp_pico::hal::sio::Sio;
     use rp_pico::hal::usb::UsbBus;
-    //use rp_pico::hal::watchdog::Watchdog;
-    //use keyberon::action::{k, l, Action, HoldTapConfig};
-    //use keyberon::chording::ChordDef;
-    //use keyberon::chording::Chording;
     use keyberon::debounce::Debouncer;
     use keyberon::key_code;
-    //use keyberon::key_code::KeyCode::*;
     use keyberon::layout::Layout;
     use keyberon::matrix::{Matrix, PressedKeys};
-    //use rp2040_hal as hal;
     use usb_device::class_prelude::*;
     //use usb_device::device::UsbDeviceState;
+    use smart_leds::{SmartLedsWrite, White, RGBW};
+    use crate::hack::Ws2812;
+    use embedded_hal::timer::CountDown;
 
     const SCAN_TIME_US: u32 = 1000;
     static mut USB_BUS: Option<usb_device::bus::UsbBusAllocator<rp_pico::hal::usb::UsbBus>> = None;
@@ -61,7 +57,7 @@ mod app {
         matrix: Matrix<DynPin, DynPin, 6, 1>,
         layout: Layout,
         #[lock_free]
-        debouncer: Debouncer<PressedKeys<6, 1>>
+        debouncer: Debouncer<PressedKeys<6, 1>>,
     }
 
     #[local]
@@ -91,31 +87,6 @@ mod app {
             &mut resets,
         );
 
-/*
-# The Physical Pins
-#                       COL0       COL1       COL2       COL3       COL4       COL5       COL6       COL7       COL8        COL9        COL10       COL11       COL12       COL13       COL14       COL15
-keyboard_cols = [ board.GP0, board.GP1, board.GP2, board.GP3, board.GP6, board.GP7, board.GP8, board.GP9, board.GP10, board.GP11, board.GP12, board.GP14, board.GP15, board.GP16, board.GP6, board.GP18 ]
-#                       ROW0        ROW1        ROW2        ROW3        ROW4
-keyboard_rows = [ board.GP19, board.GP20, board.GP21, board.GP22, board.GP26 ]
-
-# The Pin Matrix
-keyboard_cols_array = []
-keyboard_rows_array = []
-
-# Make all col pin objects inputs with pullups.
-for pin in keyboard_cols:
-    key_pin = digitalio.DigitalInOut(pin)           
-    key_pin.direction = digitalio.Direction.OUTPUT
-    key_pin.value = False
-    keyboard_cols_array.append(key_pin)
-    
-# Make all row pin objects inputs with pullups
-for pin in keyboard_rows:
-    key_pin = digitalio.DigitalInOut(pin)
-    key_pin.direction = digitalio.Direction.INPUT
-    key_pin.pull = digitalio.Pull.DOWN
-    keyboard_rows_array.append(key_pin)
-*/
         let gpio_col2 = pins.gpio2;
         let gpio_col3 = pins.gpio3;
         let gpio_col4 = pins.gpio6;
@@ -125,31 +96,102 @@ for pin in keyboard_rows:
 
         let gpio_row1 = pins.gpio20;
 
-        // 6 input pins and 1 empty pin that is not really used, but
-        // is needed by keyberon as a "row"
-        //let gpio2 = pins.gpio2;
-        //let gpio28 = pins.gpio28;
-        //let gpio3 = pins.gpio3;
-        //let gpio27 = pins.gpio27;
-        //let gpio4 = pins.gpio4;
-        //let gpio5 = pins.gpio5;
-        //let gpio26 = pins.gpio26;
-        //let gpio6 = pins.gpio6;
-        //let gpio22 = pins.gpio22;
-        //let gpio7 = pins.gpio7;
-        //let gpio10 = pins.gpio10;
-        //let gpio11 = pins.gpio11;
-        //let gpio12 = pins.gpio12;
-        //let gpio21 = pins.gpio21;
-        //let gpio13 = pins.gpio13;
-        //let gpio15 = pins.gpio15;
-        //let gpio14 = pins.gpio14;
+        let ws2812_timer = hal::timer::Timer::new(c.device.TIMER, &mut resets);
+        let mut countdown = ws2812_timer.count_down();
+        countdown.start(333.nanoseconds());
 
-        //let gpio20 = pins.gpio20;
-        
+        let mut ws2812_pin = pins.gpio13.into_push_pull_output();
+
         // delay for power on
-        for _ in 0..1000 {
+        for _ in 0..10000000 {
             cortex_m::asm::nop();
+        }
+
+        let mut sleepy = ws2812_timer.count_down();
+        sleepy.start(100.milliseconds());
+
+        let pixels = [0xF00, 0x0F0, 0x00F, 0xF00, 0x0F0, 0x00F];
+
+        //for pixel in pixels {
+        for x in 0..0xFFF {
+            let div = 4;
+
+            let mut working_pix = x;
+            
+            for _ in 0..24 {
+                if (working_pix & 1u32) == 1u32 {
+                    ws2812_pin.set_high().ok();
+                    //cortex_m::asm::delay(87 / div);
+                    cortex_m::asm::delay(60);
+                    ws2812_pin.set_low().ok();
+                    //cortex_m::asm::delay(75 / div);
+                    cortex_m::asm::delay(40);
+                }
+                else {
+                    ws2812_pin.set_high().ok();
+                    //cortex_m::asm::delay(44 / div);
+                    cortex_m::asm::delay(44 / div);
+                    ws2812_pin.set_low().ok();
+                    cortex_m::asm::delay(100 / div);
+                    //cortex_m::asm::delay(100 / div);
+                }
+                
+                working_pix = working_pix >> 1;
+            }
+
+            let _ = nb::block!(sleepy.wait());
+        }
+
+        let mut ws = Ws2812::new(countdown, ws2812_pin);
+
+        const NUM_LEDS: usize = 8;
+        let mut data = [RGBW::default(); NUM_LEDS];
+        let empty: [RGBW<u8>; NUM_LEDS] = [RGBW::default(); NUM_LEDS];
+
+        // Blink the LED's in a blue-green-red-white pattern.
+        for led in data.iter_mut().step_by(4) {
+            led.b = 0x05;
+        }
+
+        if NUM_LEDS > 1 {
+            for led in data.iter_mut().skip(1).step_by(4) {
+                led.g = 0x05;
+            }
+        }
+
+        if NUM_LEDS > 2 {
+            for led in data.iter_mut().skip(2).step_by(4) {
+                led.r = 0x05;
+            }
+        }
+
+        if NUM_LEDS > 3 {
+            for led in data.iter_mut().skip(3).step_by(4) {
+                led.a = White(0x10);
+            }
+        }
+
+        loop {
+            // let first_data = [RGBW::default(); 1];
+
+            // ws.write(first_data.iter().cloned()).unwrap();
+
+            // let mut x = ws2812_timer.count_down();
+            // x.start(1.seconds());
+            // let _ = nb::block!(x.wait());
+
+            // let second_data = [RGBW::default(); 2];
+
+            // ws.write(second_data.iter().cloned()).unwrap();
+
+            // let _ = nb::block!(x.wait());
+
+            // ws.write(data.iter().cloned()).unwrap();
+            
+            //ws.write(empty.iter().cloned()).unwrap();
+            //for _ in 0..1000 {
+            //    cortex_m::asm::nop();
+            //}
         }
 
         let matrix: Matrix<DynPin, DynPin, 6, 1> = cortex_m::interrupt::free(move |_cs| {
@@ -268,4 +310,55 @@ for pin in keyboard_rows:
             while let Ok(0) = c.shared.usb_class.lock(|k| k.write(report.as_bytes())) {}
         }
     }
+
+    /// Input a value 0 to 255 to get a color value
+    /// The colours are a transition r - g - b - back to r.
+    fn wheel(mut wheel_pos: u8) -> u32 {
+        wheel_pos = 255 - wheel_pos;
+        if wheel_pos < 85 {
+            let r: u32 = (255 - wheel_pos as u32 * 3) << 16;
+            let g: u32 = 0 << 8;
+            let b: u32 = wheel_pos as u32 * 3;
+            return r & g & b;
+        }
+        if wheel_pos < 170 {
+            wheel_pos -= 85;
+            let r: u32 = 0 << 16;
+            let g: u32 = (wheel_pos as u32 * 3) << 8;
+            let b: u32 = 255 - wheel_pos as u32 * 3;
+            return r & g & b;
+        }
+        wheel_pos -= 170;
+        let r: u32 = (wheel_pos as u32 * 3) << 16;
+        let g: u32 = (255 - wheel_pos as u32 * 3) << 8;
+        let b: u32 = 0;
+        return r & g & b;
+    }
 }
+
+
+/*
+# The Physical Pins
+#                       COL0       COL1       COL2       COL3       COL4       COL5       COL6       COL7       COL8        COL9        COL10       COL11       COL12       COL13       COL14       COL15
+keyboard_cols = [ board.GP0, board.GP1, board.GP2, board.GP3, board.GP6, board.GP7, board.GP8, board.GP9, board.GP10, board.GP11, board.GP12, board.GP14, board.GP15, board.GP16, board.GP6, board.GP18 ]
+#                       ROW0        ROW1        ROW2        ROW3        ROW4
+keyboard_rows = [ board.GP19, board.GP20, board.GP21, board.GP22, board.GP26 ]
+
+# The Pin Matrix
+keyboard_cols_array = []
+keyboard_rows_array = []
+
+# Make all col pin objects inputs with pullups.
+for pin in keyboard_cols:
+    key_pin = digitalio.DigitalInOut(pin)           
+    key_pin.direction = digitalio.Direction.OUTPUT
+    key_pin.value = False
+    keyboard_cols_array.append(key_pin)
+    
+# Make all row pin objects inputs with pullups
+for pin in keyboard_rows:
+    key_pin = digitalio.DigitalInOut(pin)
+    key_pin.direction = digitalio.Direction.INPUT
+    key_pin.pull = digitalio.Pull.DOWN
+    keyboard_rows_array.append(key_pin)
+*/
